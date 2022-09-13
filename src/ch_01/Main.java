c4b5acd804f261744597f10661fee0182f633f09
Oladapo <toktogift@gmail.com>
2022-09-13 11:54:10 +0100

    ch_o1

--- src/ch_01/Main.java
+++ src/ch_01/Main.java
@@ -2,6 +2,8 @@ package ch_01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome To Java Programming😎");
+        System.out.println("Welcome To Java 😎");
+        System.out.println("Welcome To computer science 😎");
+        System.out.println("Programming is fun 🕹");
     }
 }
